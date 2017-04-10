ae436fcd69ab1bbad2335fd2be227de0291aabf5
frankmei33 <jimei@berkeley.edu>
2017-04-09 20:39:02 -0700

    sentiment

--- Twitter+sentiment+analysis.py
+++ Twitter+sentiment+analysis.py
@@ -3,14 +3,10 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-# coding: utf-8
-
-# In[1]:
-
 # List containing positive adn negative tweets
 from nltk.corpus import twitter_samples
 pos=twitter_samples.strings('positive_tweets.json')
-neg=twitter_samples.strings('nagative_tweets.json')
+neg=twitter_samples.strings('negative_tweets.json')
 
 def review_cleaner(review):
     review = re.sub('[^a-zA-Z]',' ',review)
@@ -34,9 +30,6 @@ neg_tweets = []
 for i in range(0,len(neg)):
     neg_tweets.append((neg[i],'negative'))
 
-
-# In[3]:
-
 # Take both lists and create a single list of tuples 
 # each containing two elements. First element is an array 
 # containing the words and second element is the type of sentiment. 
@@ -46,34 +39,6 @@ tweets = []
 for (words, sentiment) in pos_tweets + neg_tweets:
     words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
     tweets.append((words_filtered, sentiment))
-
-
-# In[4]:
-
-tweets
-
-
-# In[5]:
-
-# Test tweets
-
-test_tweets = [(['feel', 'happy', 'this', 'morning'], 'positive'),
-               (['larry', 'friend'], 'positive'),
-               (['not', 'like', 'that', 'man'], 'negative'),
-               (['house', 'not', 'great'], 'negative'),
-               (['your', 'song', 'annoying'], 'negative')]
-
-
-# In[6]:
-
-test_tweets
-
-
-# ## Classifier
-
-# In[9]:
-
-import nltk
 
 
 def get_words_in_tweets(tweets):
@@ -89,22 +54,6 @@ def get_word_features(wordlist):
 
 word_features = get_word_features(get_words_in_tweets(tweets))
 
-
-# In[16]:
-
-nltk.FreqDist(get_words_in_tweets(tweets))
-
-
-# As you can see, ‘this’ is the most used word in our tweets, 
-# followed by ‘car’, followed by ‘concert’…
-# 
-# To create a classifier, we need to decide what features are relevant. 
-# To do that, we first need a feature extractor. Use on that returns a dictionary indicating what words are contained in the input passed. 
-# Here, the input is the tweet. Use the word features list defined above along with the 
-# input to create the dictionary.
-
-# In[17]:
-
 def extract_features(document):
     document_words = set(document)
     features = {}
@@ -112,33 +61,11 @@ def extract_features(document):
         features['contains(%s)' % word] = (word in document_words)
     return features
 
-
-# In[26]:
-
-# As an example, call the feature extractor with the document 
-# [‘love’, ‘this’, ‘car’] which is the first positive tweet. 
-# We obtain the following dictionary which indicates that the document 
-# contains the words: ‘love’, ‘this’ and ‘car’.
-
-    
-extract_features(['love', 'this', 'car'])
-
-
-# In[29]:
-
 # apply feature extractor to the training set
-
 training_set = nltk.classify.apply_features(extract_features, tweets)
 
-
-# In[31]:
-
 # train the classifier with the training set
-
 classifier = nltk.NaiveBayesClassifier.train(training_set)
-
-
-# In[41]:
 
 #save trained classifier
 import pickle
@@ -146,20 +73,4 @@ f = open('sentiment_classifier.pickle', 'wb')
 pickle.dump(classifier, f)
 f.close()
 
-# In[ ]:
-
-tweet = 'Larry is my friend'
-print (classifier.classify(extract_features(tweet.split())))
-
-
-# In[ ]:
-
-tweet = 'Your friend is annoying'
-print (classifier.classify(extract_features(tweet.split())))
-
-
-# In[ ]:
-
-tweet = 'My house is not great'
-print (classifier.classify(extract_features(tweet.split())))
-
+print('Classifier built and saved. Done!')
